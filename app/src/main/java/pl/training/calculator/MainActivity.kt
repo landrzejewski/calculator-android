5d81f1647dcd6546fd36e54773c87bcfa408ccbe
landrzejewski <landrzejewski.poczta@gmail.com>
2023-12-13 12:31:20 +0100

    Simple state

--- app/src/main/java/pl/training/calculator/MainActivity.kt
+++ app/src/main/java/pl/training/calculator/MainActivity.kt
@@ -3,45 +3,56 @@ package pl.training.calculator
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement.spacedBy
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Alignment.Companion.CenterVertically
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.LightGray
+import androidx.compose.ui.graphics.Color.Companion.Red
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Greeting("Android")
+            Counter()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Column(horizontalAlignment = Alignment.CenterHorizontally) {
-        Text(
-            text = "Hello $name!",
-            color = Color.Red
-        )
-        OkButton()
-    }
-}
-
-@Composable
-fun OkButton() {
-    Button(onClick = { }) {
-        Text(
-            text = "Ok"
-        )
+fun Counter() {
+    val value = remember { mutableIntStateOf(0) }
+    Column(
+        verticalArrangement = spacedBy(space = 10.dp, alignment = CenterVertically),
+        horizontalAlignment = CenterHorizontally,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Text(text = "Current value: ${value.intValue}")
+        Button(onClick = { value.intValue++ }) {
+            Text(text = "Increment")
+        }
+        Button(onClick = { value.intValue-- }) {
+            Text(text = "Decrement")
+        }
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    Greeting("Android")
+fun CounterPreview() {
+    Counter()
 }
